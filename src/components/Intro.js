e33b872aaab95718fa247e70899c0d5e90c75cdf
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-08-03 21:24:58 +0300

    add links to social media icons

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="Intro-Info">
+      <h1> Hello my name is Edwin and I am a full stack developer.</h1>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+function Navbar() {
+  return (
+    <div className="my-navbar">
+      <div className="logo">
+        <img src="./images/logo.png" alt="logo" />
+      </div>
+
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <a href="home">Home</a>
+          </li>
+          <li>
+            <a href="skills">Skills</a>
+          </li>
+          <li>
+            <a href="projects">Projects</a>
+          </li>
+        </ul>
+      </div>
+      <div class="social-container">
+        <a href="https://twitter.com/carmago_jose_">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://twitter.com/carmago_jose_">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://twitter.com/carmago_jose_">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+//    <NavLink to='/home'>Home</NavLink>
+
+export default Navbar;
