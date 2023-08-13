de22a435bddb47f6abd3a3dcfb7474dc6b014608
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-08-13 19:53:54 +0300

    add dynamic text to my career page

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../App.css";
 import Navbar from "./Navbar";
 import Intro from "./Intro";
+import Skills from "./Skills";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Skills/>
     </div>
   );
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,34 +2,58 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function Intro() {
-  const [loopNumber, setLoopNumber] = useState(0);
-  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [ loopNumber, setLoopNumber] = useState(0); 
   const [isDeleting, setIsDeleting] = useState(false);
-  const [ text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [text, setText] = useState('');
+  const toRotate = ["Web Designer","Web Developer", "UI/UX Designer"];
   const period = 2000;
+  const [delta,setDelta] = useState( 300 - Math.random() * 100 );
+  
 
-  useEffect(() =>{
-    let ticker = setInterval(() =>{
-      ticker();
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    },delta)
 
-    }, delta)
+    return () => { clearInterval(ticker)};
+  }, [text])
 
-    return() => {clearInterval(ticker);}
-  })
+  const tick = () => {
+    let i = loopNumber % toRotate.length;
+    console.log(i);
+    // console.log(toRotate.length);
+    let fullText = toRotate[i];
+    // console.log(fullText);
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
+    setText(updatedText); 
 
+    if (isDeleting){
+      setDelta(prevDelta => prevDelta /2)
+    }
+
+    if (!isDeleting && updatedText === fullText){
+      setIsDeleting(true);
+      setDelta(period);
+      
+    } else if ( isDeleting && updatedText === ''){
+      setIsDeleting(false);
+      setLoopNumber(loopNumber + 1);
+      setDelta(500);
+    }
+  }
 
   return (
     <div className="Intro-Info">
       <div className="my-picture">
-        <img src="../images/edwin.png" alt="Edwin" />
+        <img src="../images/edwin1.png" alt="Edwin" />
       </div>
 
       <div className="my-introduction">
-        <h3>HELLO I'M  </h3>
+        <button> Welcome to my Portfolio</button>
+        <h3>HELLO I'M </h3>
         <h1> Kipleting Edwin!</h1>
-        <h2>Full Stack Developer</h2>
+        <h2>Full Stack Developer <span>{text}</span></h2>
       </div>
     </div>
   );
