9a18ba6427f22376f5dd5b163353d546b5bfadad
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-08-23 18:08:11 +0300

    add an onchange functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    firstname :"",
+    lastname: "",
+    email: "",
+    phone: "",
+  })
+
+function handleChange(e){
+  setFormData({...formData,[e.target.name]: e.target.value});
+  console.log(formData);
+}
+
+function handleSubmit(){
+  console.log("Hey")
+}
+
+
   return (
     <div className="contact">
       <h1> Get In Touch</h1>
@@ -9,19 +26,19 @@ function Contact() {
         <div className="contact-inputs">
           <div className="section-1">
             <form>
-              <input type="text" placeholder="First Name"></input>
-              <input type="text" placeholder="Last Name"></input>
-              <input type="text" placeholder="Email Address"></input>
-              <input type="text" placeholder="Phone Number"></input>
+              <input type="text" name= "firstname" onChange={handleChange} placeholder="First Name"></input>
+              <input type="text" name = "lastname" onChange={handleChange}  placeholder="Last Name"></input>
+              <input type="text" name = "email" onChange={handleChange}  placeholder="Email Address"></input>
+              <input type="text" name = "phone" onChange={handleChange}  placeholder="Phone Number"></input>
             </form>
           </div>
           <div className="section-2">
-            <input type="text" placeholder="Message"></input>
+            <input type="text" name = "message" onChange={handleChange}  placeholder="Message"></input>
           </div>
         </div>
       </div>
       <div className="my-button">
-        <button>Send</button>
+        <button onClick={handleSubmit}>Send</button>
       </div>
     </div>
   );
