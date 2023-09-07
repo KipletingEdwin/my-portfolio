4052685d0ec073807fb85297d4e1e006b1fd1c3b
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-07 17:41:49 +0300

    stlyle my skill section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,8 +3,7 @@ import React from "react";
 function Skills() {
   return (
     <div className="skill-set">
-      <h2>Skills</h2>
-      <p> Loren Ipsum </p>
+      <h2> My Skills</h2>
       <div className="item">
         <img src="" alt="my-project" />
         <p> Web Development </p>
